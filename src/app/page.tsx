9c7707ae49f9ab562a9defff75a8f6604856c760
Bryan Jennings <bryanjenningz@gmail.com>
2024-05-26 15:18:34 -0700

    Add menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,27 @@ const HomePage = (): JSX.Element => {
           <span className="sr-only">Paste clipboard</span>
         </button>
       </header>
+
+      <div className="fixed inset-0 bg-black opacity-70"></div>
+
+      <aside className="fixed left-0 top-0 flex flex-col bg-black text-white">
+        <h2 className="flex h-12 items-center justify-center text-xl">Menu</h2>
+
+        <h3 className="bg-slate-800 px-4 py-1 text-sm uppercase">Reader</h3>
+        <div className="px-4 py-1 text-lg">Reader</div>
+        <div className="px-4 py-1 text-lg">Import PDF</div>
+        <div className="px-4 py-1 text-lg">Import subtitles</div>
+        <div className="px-4 py-1 text-lg">History</div>
+
+        <h3 className="bg-slate-800 px-4 py-1 text-sm uppercase">Flashcards</h3>
+        <div className="px-4 py-1 text-lg">Browse flashcards</div>
+        <div className="px-4 py-1 text-lg">Audio-only</div>
+        <div className="px-4 py-1 text-lg">Mandarin to English</div>
+        <div className="px-4 py-1 text-lg">English to Mandarin</div>
+
+        <h3 className="bg-slate-800 px-4 py-1 text-sm uppercase">Settings</h3>
+        <div className="px-4 py-1 text-lg">Settings</div>
+      </aside>
     </div>
   );
 };
