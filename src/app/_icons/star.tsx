67c4ca89a28ffd978cddf35ca1bd3f81fb1a2c54
Bryan Jennings <bryanjenningz@gmail.com>
2024-05-26 16:19:06 -0700

    Add ability to paste clipboard text

---
+++ src/app/_icons/star.tsx
@@ -0,0 +1,16 @@
+import { type SVGAttributes } from "react";
+
+export const StarIcon = (
+  attributes: SVGAttributes<SVGElement>,
+): JSX.Element => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#FFFFFF"
+    {...attributes}
+  >
+    <path d="m233-120 65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
+  </svg>
+);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,11 @@ import { useState } from "react";
 import { SideMenu } from "./_components/side-menu";
 import { ContentPasteGoIcon } from "./_icons/content-paste-go";
 import { MenuIcon } from "./_icons/menu";
+import { StarIcon } from "./_icons/star";
 
 const HomePage = (): JSX.Element => {
   const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+  const [readerText, setReaderText] = useState("");
 
   return (
     <div className="flex min-h-screen flex-col items-center bg-black text-white">
@@ -19,11 +21,38 @@ const HomePage = (): JSX.Element => {
           <span className="sr-only">Open menu</span>
         </button>
         <h1 className="grow text-center text-xl">Reader</h1>
-        <button className="flex h-12 w-12 shrink-0 items-center justify-center">
+        <button
+          className="flex h-12 w-12 shrink-0 items-center justify-center"
+          onClick={() => {
+            void (async () => {
+              try {
+                setReaderText(await navigator.clipboard.readText());
+              } catch {
+                setReaderText(prompt("Paste Mandarin text")?.trim() ?? "");
+              }
+            })();
+          }}
+        >
           <ContentPasteGoIcon />
           <span className="sr-only">Paste clipboard</span>
         </button>
       </header>
+
+      {((): JSX.Element => {
+        if (!readerText) {
+          return (
+            <article className="flex grow flex-col items-center justify-center gap-4">
+              <StarIcon width={120} height={120} />
+              <p>{`You haven't added any text.`}</p>
+              <button className="rounded-lg bg-blue-900 px-4 py-2 text-white transition hover:brightness-110">
+                Take the tutorial
+              </button>
+            </article>
+          );
+        }
+
+        return <p className="w-full max-w-2xl grow">{readerText}</p>;
+      })()}
 
       <SideMenu
         isSideMenuOpen={isSideMenuOpen}
