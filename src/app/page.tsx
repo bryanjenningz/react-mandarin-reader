71576ef306d0b0fc0f7689339aef8af460fd76bc
Bryan Jennings <bryanjenningz@gmail.com>
2024-05-26 15:02:28 -0700

    Add menu button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import { MenuIcon } from "./_icons/menu";
+
 const HomePage = (): JSX.Element => {
   return (
     <div className="flex min-h-screen flex-col items-center bg-black text-white">
-      <header>
-        <h1>Mandarin Reader</h1>
+      <header className="flex h-12 w-full max-w-2xl items-center">
+        <button className="flex h-12 w-12 shrink-0 items-center justify-center">
+          <MenuIcon />
+          <span className="sr-only">Open menu</span>
+        </button>
+        <h1 className="grow text-center text-xl">Mandarin Reader</h1>
+        <div className="flex h-12 w-12"></div>
       </header>
     </div>
   );
