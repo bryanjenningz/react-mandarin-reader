2789458ceeff1995f25a2a8db9224a98ad3bd6cf
Bryan Jennings <bryanjenningz@gmail.com>
2024-05-27 15:06:25 -0700

    Hide empty message on the first frame while Zustand is getting the persisted state from localStorage

--- src/app/_components/empty-message.tsx
+++ src/app/_components/empty-message.tsx
@@ -1,8 +1,21 @@
+import { useEffect, useState } from "react";
 import { StarIcon } from "../_icons/star";
+import { cn } from "../_utils/class-names";
 
 export const EmptyMessage = ({ message }: { message: string }): JSX.Element => {
+  const [isShown, setIsShown] = useState(false);
+
+  useEffect(() => {
+    setIsShown(true);
+  }, []);
+
   return (
-    <article className="flex grow flex-col items-center justify-center gap-4">
+    <article
+      className={cn(
+        "flex grow flex-col items-center justify-center gap-4",
+        isShown ? "opacity-100" : "opacity-0",
+      )}
+    >
       <StarIcon width={120} height={120} />
       <p>{message}</p>
       <button className="rounded-lg bg-blue-900 px-4 py-2 text-white transition hover:brightness-110">
