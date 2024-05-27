e5c717df6c6c74f71bd2af7bcdd1517730cc4278
Bryan Jennings <bryanjenningz@gmail.com>
2024-05-27 15:47:44 -0700

    Make each reader page have a fixed size

--- src/app/_components/reader-text.tsx
+++ src/app/_components/reader-text.tsx
@@ -9,7 +9,17 @@ export const ReaderText = ({
   readerText: string;
   readerDate: number;
 }): JSX.Element => {
-  const [selection, setSelection] = useState(-1);
+  const [selection, setSelection] = useState<{ x: number; y: number } | null>(
+    null,
+  );
+  const charsPerLine = 14;
+  const linesPerPage = 14;
+  const charsPerPage = charsPerLine * linesPerPage;
+  const pageIndex = 0;
+  const pageText = readerText.slice(
+    pageIndex * charsPerPage,
+    (pageIndex + 1) * charsPerPage,
+  );
 
   if (!readerText) {
     return <EmptyMessage message="You haven't added any text." />;
@@ -17,20 +27,39 @@ export const ReaderText = ({
 
   return (
     <section className="max-h-[70vh] w-full max-w-2xl grow overflow-auto px-2 text-center text-2xl">
-      {readerText.split("").map((char, i) => {
+      {chunk(pageText.split(""), charsPerLine).map((line, y) => {
         return (
-          <button
-            key={`${readerDate}-${i}`}
-            className={cn(
-              "inline-flex h-8 w-6 items-center justify-center",
-              selection === i && "bg-blue-600",
-            )}
-            onClick={() => setSelection(i)}
-          >
-            {char}
-          </button>
+          <div key={`${readerDate}-${y}`}>
+            {line.map((char, x) => {
+              return (
+                <button
+                  key={`${readerDate}-${y}-${x}`}
+                  className={cn(
+                    "inline-flex h-8 w-6 items-center justify-center",
+                    selection?.x === x && selection.y === y && "bg-blue-600",
+                  )}
+                  onClick={() => setSelection({ x, y })}
+                >
+                  {char}
+                </button>
+              );
+            })}
+          </div>
         );
       })}
     </section>
   );
+};
+
+const chunk = <T,>(values: T[], size: number): T[][] => {
+  const chunks: T[][] = [];
+  let lastChunk: T[] = [];
+  for (const [i, value] of values.entries()) {
+    lastChunk.push(value);
+    if (lastChunk.length >= size || i === values.length - 1) {
+      chunks.push(lastChunk);
+      lastChunk = [];
+    }
+  }
+  return chunks;
 };
