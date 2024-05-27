05f56e45be725867306bd4ee17ca0a7f352981eb
Bryan Jennings <bryanjenningz@gmail.com>
2024-05-26 21:50:05 -0700

    Use state store for reader text state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,24 @@ import { useState } from "react";
 import { SideMenu } from "./_components/side-menu";
 import { ReaderHeader } from "./_components/reader-header";
 import { ReaderText } from "./_components/reader-text";
+import { useStateStore } from "./_stores/state";
 
 const HomePage = (): JSX.Element => {
   const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
-  const [readerText, setReaderText] = useState("");
+  const readerText = useStateStore((state) => state.readerText);
+  const dispatch = useStateStore((state) => state.dispatch);
 
   return (
     <div className="flex min-h-screen flex-col items-center bg-black text-white">
       <ReaderHeader
         setIsSideMenuOpen={setIsSideMenuOpen}
-        setReaderText={setReaderText}
+        setReaderText={(readerText) => {
+          dispatch({
+            type: "PASTE_READER_TEXT",
+            text: readerText,
+            date: Date.now(),
+          });
+        }}
       />
 
       <ReaderText readerText={readerText} />
