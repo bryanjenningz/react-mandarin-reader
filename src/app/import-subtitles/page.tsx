d3c5a1092a77b40ca149beabc7c3b623c9e25bdc
Bryan Jennings <bryanjenningz@gmail.com>
2024-05-28 15:29:11 -0700

    Add import subtitle file preview

--- src/app/import-subtitles/page.tsx
+++ src/app/import-subtitles/page.tsx
@@ -7,6 +7,7 @@ import { ImportSubtitlesHeader } from "./_components/import-subtitles-header";
 
 const ImportSubtitlesPage = (): JSX.Element => {
   const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+  const [subtitlesText, setSubtitlesText] = useState("");
 
   return (
     <div className="flex min-h-screen flex-col items-center bg-black text-white">
@@ -14,8 +15,33 @@ const ImportSubtitlesPage = (): JSX.Element => {
 
       <div className="flex grow flex-col items-center justify-center gap-2">
         <SubtitlesIcon width={120} height={120} />
+        <input
+          type="file"
+          className="max-w-80 rounded-lg bg-blue-900 px-4 py-2 text-white transition hover:brightness-110"
+          onChange={(event) => {
+            if (!event.target.files?.[0]) return;
+            const fileReader = new FileReader();
+            fileReader.readAsText(event.target.files[0]);
+            fileReader.onload = () => {
+              const fileText = fileReader.result;
+              if (typeof fileText === "string") {
+                setSubtitlesText(fileText);
+              }
+            };
+          }}
+        />
+
+        {subtitlesText && (
+          <div className="flex flex-col items-center gap-2 py-4">
+            <h2>Preview</h2>
+            <div className="line-clamp-4 overflow-auto px-4 text-slate-400">
+              {subtitlesText}
+            </div>
+          </div>
+        )}
+
         <button className="rounded-lg bg-blue-900 px-4 py-2 text-white transition hover:brightness-110">
-          Import subtitles
+          Save subtitles
         </button>
       </div>
 
