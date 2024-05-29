753a69758696e1f6e976b493024a4681ffae9b4e
Bryan Jennings <bryanjenningz@gmail.com>
2024-05-29 12:48:06 -0700

    Only load dictionary once

--- src/app/_stores/dictionary.ts
+++ src/app/_stores/dictionary.ts
@@ -6,9 +6,10 @@ type DictionaryStore = {
   loadDictionary: () => void;
 };
 
-export const useDictionaryStore = create<DictionaryStore>()((set) => ({
+export const useDictionaryStore = create<DictionaryStore>()((set, get) => ({
   dictionary: { traditional: [], simplified: [] },
   loadDictionary: () => {
+    if (get().dictionary.traditional.length > 0) return;
     void (async () => {
       const dictionary = await loadDictionary();
       set({ dictionary });
