db2c8e0fe2fcb8a5acf2274cb614c7f249dfbd39
Bryan Jennings <bryanjenningz@gmail.com>
2024-05-30 14:38:43 -0700

    Use FlashcardPage in MandarinToEnglishFlashcardsPage

--- src/app/mandarin-to-english-flashcards/page.tsx
+++ src/app/mandarin-to-english-flashcards/page.tsx
@@ -2,21 +2,35 @@
 
 import { useEffect, useState } from "react";
 import { useStateStore } from "~/app/_stores/state";
-import { SideMenu } from "~/app/_components/side-menu";
-import { EmptyMessage } from "../_components/empty-message";
 import { textToSpeech } from "../_utils/text-to-speech";
 import { VolumeUpIcon } from "../_icons/volume-up";
-import { SimpleHeader } from "../_components/simple-header";
+import { FlashcardPage } from "../_components/flashcard-page";
+import { type DictionaryEntry } from "../_utils/dictionary";
+
+const front = (entry: DictionaryEntry): JSX.Element => (
+  <div className="flex gap-2 text-3xl">
+    <div>{entry.traditional}</div>
+    {entry.traditional !== entry.simplified && <div>{entry.simplified}</div>}
+  </div>
+);
+
+const back = (entry: DictionaryEntry): JSX.Element => (
+  <>
+    <div className="text-lg">{entry.pinyin}</div>
+    <button onClick={() => textToSpeech(entry.simplified)}>
+      <VolumeUpIcon width={60} height={60} />
+      <span className="sr-only">Play audio</span>
+    </button>
+    <div>{entry.meanings.join(", ")}</div>
+  </>
+);
 
 const MandarinToEnglishFlashcardsPage = (): JSX.Element => {
-  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
   const [isFlashcardBackShown, setIsFlashcardBackShown] = useState(false);
-  const flashcards = useStateStore((state) => state.flashcards);
-  const firstFlashcard = flashcards[0];
+  const firstFlashcard = useStateStore((x) => x.flashcards[0]);
   const playAudioOnFlashcardBackEnabled = useStateStore(
     (x) => x.settings.playAudioOnFlashcardBack.enabled,
   );
-  const dispatch = useStateStore((x) => x.dispatch);
 
   useEffect(() => {
     if (
@@ -29,98 +43,13 @@ const MandarinToEnglishFlashcardsPage = (): JSX.Element => {
   }, [firstFlashcard, isFlashcardBackShown, playAudioOnFlashcardBackEnabled]);
 
   return (
-    <div className="flex min-h-screen flex-col items-center bg-black text-white">
-      <SimpleHeader
-        title="Mandarin Flashcards"
-        setIsSideMenuOpen={setIsSideMenuOpen}
-      />
-
-      {((): JSX.Element => {
-        if (!firstFlashcard) {
-          return <EmptyMessage message="You haven't saved any flashcards." />;
-        }
-
-        return (
-          <div className="flex w-full max-w-2xl grow flex-col">
-            <div className="flex w-full grow flex-col items-center gap-2">
-              <div className="flex gap-2 text-3xl">
-                <div>{firstFlashcard.entry.traditional}</div>
-                {firstFlashcard.entry.traditional !==
-                  firstFlashcard.entry.simplified && (
-                  <div>{firstFlashcard.entry.simplified}</div>
-                )}
-              </div>
-
-              {((): JSX.Element => {
-                if (isFlashcardBackShown) {
-                  return (
-                    <>
-                      <div className="text-lg">
-                        {firstFlashcard.entry.pinyin}
-                      </div>
-                      <button
-                        onClick={() =>
-                          textToSpeech(firstFlashcard.entry.simplified)
-                        }
-                      >
-                        <VolumeUpIcon width={60} height={60} />
-                        <span className="sr-only">Play audio</span>
-                      </button>
-                      <div>{firstFlashcard.entry.meanings.join(", ")}</div>
-                    </>
-                  );
-                }
-
-                return <></>;
-              })()}
-            </div>
-            <div className="flex h-12 w-full">
-              {((): JSX.Element => {
-                if (isFlashcardBackShown) {
-                  return (
-                    <>
-                      <button
-                        className="flex grow basis-1 items-center justify-center bg-red-900 text-lg hover:brightness-110"
-                        onClick={() => {
-                          dispatch({ type: "FAIL_FLASHCARD" });
-                          setIsFlashcardBackShown(false);
-                        }}
-                      >
-                        Fail
-                      </button>
-                      <button
-                        className="flex grow basis-1 items-center justify-center bg-blue-900 text-lg hover:brightness-110"
-                        onClick={() => {
-                          dispatch({ type: "PASS_FLASHCARD" });
-                          setIsFlashcardBackShown(false);
-                        }}
-                      >
-                        Pass
-                      </button>
-                    </>
-                  );
-                }
-
-                return (
-                  <button
-                    className="flex grow basis-1 items-center justify-center bg-blue-900 text-lg hover:brightness-110"
-                    onClick={() => setIsFlashcardBackShown(true)}
-                  >
-                    Show back
-                  </button>
-                );
-              })()}
-            </div>
-          </div>
-        );
-      })()}
-
-      <SideMenu
-        selectedItem="Mandarin to English"
-        isSideMenuOpen={isSideMenuOpen}
-        setIsSideMenuOpen={setIsSideMenuOpen}
-      />
-    </div>
+    <FlashcardPage
+      selectedMenuItem="Mandarin to English"
+      isFlashcardBackShown={isFlashcardBackShown}
+      setIsFlashcardBackShown={setIsFlashcardBackShown}
+      front={front}
+      back={back}
+    />
   );
 };
 
