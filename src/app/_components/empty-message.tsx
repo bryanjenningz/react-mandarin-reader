d09f263e37b0a9b986af39e2537b25c898c7d71b
Bryan Jennings <bryanjenningz@gmail.com>
2024-05-31 15:51:03 -0700

    Add link to tutorial in empty message

--- src/app/_components/empty-message.tsx
+++ src/app/_components/empty-message.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
+import Link from "next/link";
 import { StarIcon } from "../_icons/star";
 import { cn } from "../_utils/class-names";
+import { useStateStore } from "../_stores/state";
 
 export const EmptyMessage = ({ message }: { message: string }): JSX.Element => {
   // We hide the empty message on the first render while Zustand is getting the
   // persisted state from localStorage.
   const [isShown, setIsShown] = useState(false);
+  const dispatch = useStateStore((x) => x.dispatch);
 
   useEffect(() => {
     setIsShown(true);
@@ -20,9 +23,13 @@ export const EmptyMessage = ({ message }: { message: string }): JSX.Element => {
     >
       <StarIcon width={120} height={120} />
       <p>{message}</p>
-      <button className="rounded-lg bg-blue-900 px-4 py-2 text-white transition hover:brightness-110">
+      <Link
+        href="/tutorial"
+        className="rounded-lg bg-blue-900 px-4 py-2 text-white transition hover:brightness-110"
+        onClick={() => dispatch({ type: "CLOSE_MENU" })}
+      >
         Take the tutorial
-      </button>
+      </Link>
     </article>
   );
 };
