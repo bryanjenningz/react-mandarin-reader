f858c8bfd3b957d18be2eeacb33dd93e7f21323b
Bryan Jennings <bryanjenningz@gmail.com>
2024-05-31 15:06:00 -0700

    Add manifest link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Mandarin Reader",
   description:
     "A Mandarin Chinese PDF, subtitle, and clipboard text reader with a popup dictionary and built-in spaced-repetition flashcards.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+  manifest: "/app.webmanifest",
 };
 
 export default function RootLayout({
