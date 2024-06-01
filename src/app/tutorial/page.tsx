81e2efddf4ebf6e9dc6863a8e67dde11ed3e66c0
Bryan Jennings <bryanjenningz@gmail.com>
2024-06-01 10:18:10 -0700

    Add start page of the tutorial

--- src/app/tutorial/page.tsx
+++ src/app/tutorial/page.tsx
@@ -1,15 +1,19 @@
 "use client";
 
-import { SideMenu } from "~/app/_components/side-menu";
-import { SimpleHeader } from "../_components/simple-header";
+import HomePage from "../page";
 
 const TutorialPage = (): JSX.Element => {
   return (
-    <div className="flex min-h-screen flex-col items-center bg-black text-white">
-      <SimpleHeader title="Tutorial" />
+    <>
+      <HomePage />
 
-      <SideMenu selectedItem="Tutorial" />
-    </div>
+      <div className="fixed inset-0 z-50 flex flex-col items-center justify-end gap-4 bg-black bg-opacity-80 p-4 text-center text-2xl text-white md:justify-center">
+        <p className="w-full max-w-2xl text-center text-2xl">{`Welcome to the tutorial. We'll go over how to use this web app to learn Mandarin.`}</p>
+        <button className="w-full max-w-2xl rounded-full bg-blue-700 px-4 py-2 text-white hover:brightness-110">
+          Click to start
+        </button>
+      </div>
+    </>
   );
 };
 
